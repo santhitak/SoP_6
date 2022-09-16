d1c4f3aa54b0fdf6f7b23ac90657901e8764d4cf
santhitak <santhita.sk@gmail.com>
2022-09-16 16:36:48 +0700

    feat: Wizards.java

--- src/main/java/com/example/lab06service/pojo/Wizards.java
+++ src/main/java/com/example/lab06service/pojo/Wizards.java
@@ -1,4 +1,12 @@
 package com.example.lab06service.pojo;
 
+
+import java.util.ArrayList;
+
 public class Wizards {
+    private ArrayList<Wizard> wizards;
+
+    public Wizards(){
+        wizards = new ArrayList<>();
+    }
 }
