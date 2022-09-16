8df626595e2ae0917d6018a1118bbfd8c7095487
santhitak <santhita.sk@gmail.com>
2022-09-16 16:35:59 +0700

    feat: WizardController.java

--- src/main/java/com/example/lab06service/controller/WizardController.java
+++ src/main/java/com/example/lab06service/controller/WizardController.java
@@ -1,4 +1,45 @@
 package com.example.lab06service.controller;
 
+import com.example.lab06service.pojo.Wizard;
+import com.example.lab06service.repo.WizardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class WizardController {
+    @Autowired
+    WizardService wizService;
+    Wizard newWizard;
+
+    public WizardController(){}
+
+    @GetMapping("/wizards")
+    public ResponseEntity<?> getWiz(){
+        List<Wizard> wiz = wizService.getWiz();
+        System.out.println(wiz);
+        return ResponseEntity.ok(wiz);
+    }
+
+    @PostMapping("/addWizard")
+    public ResponseEntity<Wizard> addWiz(@RequestBody Wizard wizzy){
+        Wizard wiz = wizService.addWiz(wizzy);
+        System.out.println(wizzy);
+        return ResponseEntity.ok(wiz);
+    }
+
+    @PostMapping("/updateWizard")
+    public ResponseEntity<Wizard> updateWiz(@RequestBody Wizard wizzy){
+        Wizard wiz = wizService.updateWiz(wizzy);
+        System.out.println(wizzy);
+        return ResponseEntity.ok(wiz);
+    }
+
+    @PostMapping("/deleteWizard")
+    public ResponseEntity<Boolean> delWiz(@RequestBody Wizard wizzy){
+        boolean wiz = wizService.delWiz(wizzy);
+        return ResponseEntity.ok(true);
+    }
 }
