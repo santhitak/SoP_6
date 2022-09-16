1eafa693214cf34bd5001bf573d7717820a4584e
santhitak <santhita.sk@gmail.com>
2022-09-17 01:43:01 +0700

    add: getter setter

--- src/main/java/com/example/lab06service/pojo/Wizards.java
+++ src/main/java/com/example/lab06service/pojo/Wizards.java
@@ -2,11 +2,20 @@ package com.example.lab06service.pojo;
 
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Wizards {
-    private ArrayList<Wizard> wizards;
+    private List<Wizard> wizards;
 
     public Wizards(){
-        wizards = new ArrayList<>();
+        this.wizards = new ArrayList<>();
+    }
+
+    public List<Wizard> getWiz(){
+        return wizards;
+    }
+
+    public void setWiz(List<Wizard> wizards){
+        this.wizards = wizards;
     }
 }
