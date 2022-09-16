6b31384034d17c1d51919ec54702008a7b9e690e
santhitak <santhita.sk@gmail.com>
2022-09-16 16:36:26 +0700

    feat: WizardService.java

--- src/main/java/com/example/lab06service/repo/WizardService.java
+++ src/main/java/com/example/lab06service/repo/WizardService.java
@@ -1,4 +1,40 @@
 package com.example.lab06service.repo;
 
+import com.example.lab06service.pojo.Wizard;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class WizardService {
+    @Autowired
+    private WizardRepo repo;
+
+    public WizardService() {}
+
+    public WizardService(WizardRepo repo){
+        this.repo = repo;
+    }
+
+    public List<Wizard> getWiz(){
+        return repo.findAll();
+    }
+
+    public Wizard addWiz(Wizard wiz){
+        return repo.save(wiz);
+    }
+
+    public Wizard updateWiz(Wizard wiz){
+        return repo.save(wiz);
+    }
+
+    public boolean delWiz(Wizard wiz){
+        try{
+            repo.delete(wiz);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
 }
